5f0251cae94c27f1f70045740f83bf13a1230ab7
Jack Nash <jacknash42@gmail.com>
2023-09-01 11:44:34 +1000

    initial commit

---
+++ src/background.ts
@@ -0,0 +1,3 @@
+chrome.runtime.onMessage.addListener((request, sender) => {});
+
+// This is where the fetching will be done as this occurs off browser
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+$(function () {
+	console.log('Mafia Engine has loaded.');
+	console.log('This should only be called on forum.mafiascum.net');
+});
