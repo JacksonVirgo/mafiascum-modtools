8e22e8ffdfaa030c1c7b88e21fc71460c26d51c1
Jack Nash <jacknash42@gmail.com>
2023-09-01 14:00:31 +1000

    Make the background script more modular

--- src/background.ts
+++ src/background.ts
@@ -2,30 +2,37 @@ import browser from 'webextension-polyfill';
 import { load } from 'cheerio';
 import { PageDataResponse, isPageDataRequest } from './types/pageData';
 
+function sendResponse(response: PageDataResponse) {
+	return Promise.resolve(response);
+}
+
 browser.runtime.onMessage.addListener(async (request) => {
-	const sendResponse = (response: PageDataResponse) => {
-		return Promise.resolve(response);
-	};
-
-	if (!isPageDataRequest(request)) return sendResponse({ status: 400, message: 'Invalid request' });
-
 	try {
-		const response = await fetch(request.url);
-		if (response.status !== 200) return Promise.resolve({ status: response.status, message: response.statusText });
-
-		const html = await response.text();
-
-		const $ = load(html);
-		const title = $('h2').first().text();
-
-		if (!title) return sendResponse({ status: 500, message: 'Could not find page title.' });
-
-		return sendResponse({
-			status: 200,
-			pageTitle: title,
-		});
+		switch (true) {
+			case isPageDataRequest(request):
+				return await fetchPageData(request.url);
+			default:
+				return sendResponse({ status: 400, message: 'Invalid request' });
+		}
 	} catch (err) {
 		console.log(err); // Do proper error handling later
 		return sendResponse({ status: 500, message: 'Unexpected error' });
 	}
 });
+
+async function fetchPageData(url: string) {
+	const response = await fetch(url);
+	if (response.status !== 200) return Promise.resolve({ status: response.status, message: response.statusText });
+
+	const html = await response.text();
+
+	const $ = load(html);
+	const title = $('h2').first().text();
+
+	if (!title) return sendResponse({ status: 500, message: 'Could not find page title.' });
+
+	return sendResponse({
+		status: 200,
+		pageTitle: title,
+	});
+}
