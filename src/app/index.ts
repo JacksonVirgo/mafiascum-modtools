408271ff6098c24c138a7578148f1b3bd7513887
Jack Nash <jacknash42@gmail.com>
2023-09-21 08:10:14 +1000

    remove template for the VC button

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,24 +1,19 @@
 import $ from 'jquery';
 import { CSS_HIDDEN, createModal } from './modal';
-import { getTemplate } from './request';
 
 $(async function () {
 	const modal = await createModal();
 	if (!modal) return;
 
 	$('body').append(modal);
-
-	const vcButton = await getTemplate('vcButton');
-	if (vcButton) {
-		$('.author').each((_index, element) => {
-			const button = $(vcButton);
-			button
-				.find('button')
-				.first()
-				.on('click', async () => {
-					modal.removeClass(CSS_HIDDEN);
-				});
-			$(element).find('a:nth-child(3)').after(button);
-		});
-	}
+	$('.author').each((_index, element) => {
+		const button = $('<span class="mafia-engine-vc"> - <button>VC</button></span>');
+		button
+			.find('button')
+			.first()
+			.on('click', async () => {
+				modal.removeClass(CSS_HIDDEN);
+			});
+		$(element).find('a:nth-child(3)').after(button);
+	});
 });
