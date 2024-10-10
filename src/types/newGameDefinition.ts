d3d97ac941073f08fd1d10ac6b38cc1b947bc315
Jack Nash <jacknash42@gmail.com>
2024-10-10 13:04:52 +1000

    refactor: split game def schema into subschemas

--- src/types/newGameDefinition.ts
+++ src/types/newGameDefinition.ts
@@ -2,29 +2,97 @@
 
 import { z } from 'zod';
 
+export const DaySchema = z.object({
+	dayNumber: z.number(),
+	startPost: z.number(),
+	endPost: z.number().optional(),
+});
+
+export function isDay(obj: unknown): obj is z.infer<typeof DaySchema> {
+	try {
+		const parse = DaySchema.parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+export function isDayArray(obj: unknown): obj is z.infer<typeof DaySchema>[] {
+	try {
+		const parse = DaySchema.array().parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+export const PlayerSchema = z.object({
+	current: z.string(),
+	previous: z.string().array(),
+	aliases: z.string().array(),
+	diedAt: z.number().optional(),
+});
+
+export function isPlayer(obj: unknown): obj is z.infer<typeof PlayerSchema> {
+	try {
+		const parse = PlayerSchema.parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+export function isPlayerArray(
+	obj: unknown,
+): obj is z.infer<typeof PlayerSchema>[] {
+	try {
+		const parse = PlayerSchema.array().parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+export const VoteSchema = z.object({
+	postNumber: z.number(),
+	ignore: z.boolean().optional(), // Used to have the tool ignore this vote
+	target: z.string().nullish(), // Used to fix who the target is if an issue arises
+});
+
+export function isVote(obj: unknown): obj is z.infer<typeof VoteSchema> {
+	try {
+		const parse = VoteSchema.parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
+export function isVoteArray(obj: unknown): obj is z.infer<typeof VoteSchema>[] {
+	try {
+		const parse = VoteSchema.array().parse(obj);
+		if (parse) return true;
+		return false;
+	} catch (err) {
+		console.log(err);
+		return false;
+	}
+}
+
 export const GameDefinitionSchema = z.object({
-	days: z
-		.object({
-			dayNumber: z.number(),
-			startPost: z.number(),
-			endPost: z.number().optional(),
-		})
-		.array(),
-	players: z
-		.object({
-			current: z.string(),
-			previous: z.string().array(),
-			aliases: z.string().array(),
-			diedAt: z.number().optional(),
-		})
-		.array(),
-	votes: z
-		.object({
-			postNumber: z.number(),
-			ignore: z.boolean().optional(), // Used to have the tool ignore this vote
-			target: z.string().nullish(), // Used to fix who the target is if an issue arises
-		})
-		.array(),
+	days: DaySchema.array(),
+	players: PlayerSchema.array(),
+	votes: VoteSchema.array(),
 });
 
 export type GameDefinition = z.infer<typeof GameDefinitionSchema>;
