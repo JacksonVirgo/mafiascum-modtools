1b3b28bb71fe182631173873ebc48079bb0a6fdd
Jack Nash <jacknash42@gmail.com>
2024-10-11 20:19:52 +1000

    fix: textarea sizing issues now resolved

--- src/content/components/form/TextArea.tsx
+++ src/content/components/form/TextArea.tsx
@@ -6,9 +6,9 @@ interface TextAreaProps {
 	placeholder?: string;
 	defaultValue?: string;
 	onChange?: (value: string) => void;
-
 	className?: string;
 	withoutLabel?: boolean;
+	readOnly?: boolean;
 }
 
 export default function TextArea({
@@ -17,26 +17,29 @@ export default function TextArea({
 	placeholder,
 	defaultValue,
 	withoutLabel = false,
+	readOnly = false,
 	className = '',
 	onChange,
 }: TextAreaProps) {
 	return (
-		<div className={`flex flex-col box-border p-0 m-0 ${className}`}>
+		<div className={`flex flex-col box-border p-0 m-0 gap-2 ${className}`}>
 			{!withoutLabel && (
 				<label htmlFor={name} className="text-base w-full">
 					{label}
 				</label>
 			)}
+
 			<textarea
 				id={name}
 				name={name}
 				placeholder={placeholder}
 				defaultValue={defaultValue}
-				className="p-2 grow w-4/5 max-w-4/5 !bg-primary-lighter text-white rounded-sm border border-primary-lightest hover:!border-primary-lightest focus:!border-secondary-color focus:outline-none resize-none"
+				className="!p-2 grow h-full w-full box-border !bg-primary-lighter text-white rounded-sm border border-primary-lightest hover:!border-primary-lightest focus:!border-secondary-color focus:outline-none resize-none"
 				onChange={(e) => {
 					if (!onChange) return;
 					onChange(e.target.value);
 				}}
+				readOnly={readOnly}
 			></textarea>
 		</div>
 	);
