8627f6778140357670fb7761bfb9497cb4c2d530
JacksonVirgo <jacknash42@gmail.com>
2024-10-27 21:47:00 +1000

    chore: use "unknown" instead of "any"

--- src/lib/hooks/useInitial.tsx
+++ src/lib/hooks/useInitial.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 
-export type UseInitialProps = () => Promise<any> | any;
+export type UseInitialProps = () => Promise<unknown> | unknown;
 export function useInitial(func: UseInitialProps) {
 	return useEffect(() => {
 		func();
